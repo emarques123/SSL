5d47c1714d7eb691d4a11581a4b5afb6b27c9453
Enrique Marques <emarques@frba.utn.edu.ar>
2024-04-12 21:42:00 -0300

    fuente corregida para que compile y ejecute

--- Clases/Clase3_baseEtapas/baseEtapas.c
+++ Clases/Clase3_baseEtapas/baseEtapas.c
@@ -2,5 +2,6 @@
 #define CARGA_MAXIMA 1500
 int/*medio*/main(void){
 int i=42;
-prontf("La respuesta es %d\n");
-prontf("el valor %d\n",CARGA_MAXIMA);
+printf("La respuesta es %d\n");
+printf("el valor %d\n",CARGA_MAXIMA);
+}
